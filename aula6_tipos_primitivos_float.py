1ce12e50ab81ed6cdc6ce563b1a1e28fc9f872e8
Jaqueline Mangabeira <94205108+JaquelineMangabeira@users.noreply.github.com>
2022-01-25 21:15:30 -0300

    Add files via upload

---
+++ aula6_tipos_primitivos_float.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+n3 = float(input('Mais uma vez, outro número: '))
+soma = n1+n2+n3
+print(f'A soma entre {n1},{n2} e {n3} é igual a {soma}.')
---
+++ exercicio_001.py
@@ -0,0 +1,2 @@
+mensagem_fim_de_ano = 'Quero férias'
+print(mensagem_fim_de_ano)
